15ae602f51f9e85e67803cc001927648c2a85f70
Percy-Coding <55159839+Percy-Coding@users.noreply.github.com>
2021-11-21 19:13:40 -0500

    Diagnose with Real Values

--- coviddetector/src/views/Diagnose.vue
+++ coviddetector/src/views/Diagnose.vue
@@ -5,101 +5,79 @@
         <v-form action="">
             <v-row>
               <v-col
+                v-for="symptom in symptoms"
+                :key="symptom"
                 cols="12"
                 sm="4"
                 md="4"
               >
                  <v-checkbox
-                  v-model="ex4"
-                  label="Tos"
+                  v-model="checks"
+                  :label="symptom"
                   color="primary"
-                  value="Tos"
-                  hide-details
-                ></v-checkbox>
-              </v-col>
-              <v-col
-                cols="12"
-                sm="4"
-                md="4"
-              >
-                 <v-checkbox
-                  v-model="ex4"
-                  label="Gripe"
-                  color="primary"
-                  value="Gripe"
-                  hide-details
-                ></v-checkbox>
-              </v-col>
-              <v-col
-                cols="12"
-                sm="4"
-                md="4"
-              >
-               <v-checkbox
-                  v-model="ex4"
-                  label="Fiebre"
-                  color="primary"
-                  value="Fiebre"
+                  :value="symptom"
                   hide-details
                 ></v-checkbox>
               </v-col>
             </v-row>
-            <v-row class="mt-12">
-              <v-col
-                cols="12"
-                sm="4"
-                md="4"
-              >
-                <v-checkbox
-                  v-model="ex4"
-                  label="Dolor de Pecho"
-                  color="primary"
-                  value="Dolor-Pecho"
-                  hide-details
-                ></v-checkbox>
+            <v-row class="text-center my-4 py-4">
+              <v-col>
+                <v-btn class="btn btn-primary" @click="submitForm()">
+                  <span>Get Results</span>
+                </v-btn>
               </v-col>
-              <v-col
-                cols="12"
-                sm="4"
-                md="4"
-              >
-                <v-checkbox
-                  v-model="ex4"
-                  label="XXXX"
-                  color="primary"
-                  value="XXXX"
-                  hide-details
-                ></v-checkbox>
-              </v-col>
-              <v-col
-                cols="12"
-                sm="4"
-                md="4"
-              >
-                 <v-checkbox
-                  v-model="ex4"
-                  label="YYYY"
-                  color="primary"
-                  value="YYYY"
-                  hide-details
-                ></v-checkbox>
-              </v-col>
-            </v-row>
+            </v-row> 
         </v-form>
       </v-container>
     </v-card-text>
-    <v-container>
-        {{ex4}}
+    <v-container v-model="submitted">
+      <p class="font-weight-bold">
+        Diagnostico: {{body}}
+      </p>   
     </v-container>
   </v-card>
 </template>
 
 <script>
+  //import axios from 'axios';
   export default {
     data () {
       return {
-        ex4: ["ZZZZ"],
+        checks: ["placeholder"],
+        symptoms: ["Tos","Cefalea","Congestion Nasal","Dificultad Respiratoria",
+        "Dolor de garganta","Fiebre","Diarrea","Nauseas","Anosmia Hiposmia","Dolor abdominal",
+        "Dolor de articulaciones","Dolor Muscular","Dolor de pecho","Otros sintomas"]   ,
+        result: [],
+        submitted: false,
+        body: [],     
       }
     },
+    methods:{
+      compareResults(){
+        let vm = this;
+        vm.body = [];
+        vm.symptoms.forEach(function(value){
+          if(vm.checks.includes(value)){
+            vm.body.push(1);
+          }
+          else vm.body.push(0);
+        });
+      },
+      submitForm(){
+        this.submitted = true;
+        this.compareResults(); 
+        this.body = {input: this.body};
+        /*axios.post('http://localhost:8083/predict-covid',this.body)
+        .then((res) =>{
+          this.result.push(res);
+        })
+        .catch((error) => {
+          this.result.push(error);
+        })
+        .finally(() =>{
+          this.submitted = true;
+        })*/
+      }
+    }
   }
 </script>
